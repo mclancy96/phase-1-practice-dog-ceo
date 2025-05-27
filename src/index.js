1c7e5647f06600d87d5106285a59bd6dee5c1c28
Mike <mclancy96@gmail.com>
2025-05-27 08:22:02 -0400

    Add dog images on load

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+document.addEventListener('DOMContentLoaded', () => {
+  const dogContainer = document.querySelector('#dog-image-container')
+  fetch(imgUrl)
+    .then(response => response.json())
+    .then(data => {
+      const images = data.message
+      images.forEach((image) => {
+        const imageEl = document.createElement('img')
+        imageEl.src = image
+        dogContainer.appendChild(imageEl)
+      })
+    })
+    .catch(error => console.error('Error fetching dog images:', error));
+})
