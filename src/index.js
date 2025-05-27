f56a02b59942164f7ae4ac8e7f6e8c3f4769dd46
Mike <mclancy96@gmail.com>
2025-05-27 08:55:16 -0400

    Add breed lis to ul

--- src/index.js
+++ src/index.js
@@ -1,20 +1,48 @@
-const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
 document.addEventListener('DOMContentLoaded', () => {
-  addDogs()
+  addDogs();
+  addBreeds();
 })
 
 const addDogs = () => {
-  const dogContainer = document.querySelector('#dog-image-container')
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
   fetch(imgUrl)
     .then(response => response.json())
     .then(data => {
       const images = data.message
-      images.forEach((image) => {
-        const imageEl = document.createElement('img')
-        imageEl.src = image
-        dogContainer.appendChild(imageEl)
-      })
+      images.forEach(addDogImage)
     })
     .catch(error => console.error('Error fetching dog images:', error));
+}
 
+const addDogImage = (image) => {
+  const dogContainer = document.querySelector('#dog-image-container')
+  const imageEl = document.createElement('img')
+  imageEl.src = image
+  dogContainer.appendChild(imageEl)
+}
+
+const addBreeds = () => {
+  const breedUrl = "https://dog.ceo/api/breeds/list/all";
+  fetch(breedUrl)
+    .then(resp => resp.json())
+    .then(data => {
+      const breeds = data.message;
+      Object.entries(breeds).forEach(parseBreedElements)
+    })
+}
+
+const parseBreedElements = (breed) => {
+  if (breed[1].length > 0) {
+    breed[1].forEach(subname => addBreedElement(`${breed[0]} - ${subname}`))
+  } else {
+    console.log('im in the else')
+    addBreedElement(breed[0])
+  }
+}
+
+const addBreedElement = (breedName) => {
+  const breedList = document.querySelector('#dog-breeds')
+  const breedEl = document.createElement('li');
+  breedEl.textContent = breedName;
+  breedList.appendChild(breedEl)
 }
